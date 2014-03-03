0c49b73de69c5e9d10bb7332ec49459754b5c4ec
menor <josemenorgmail.com>
2014-03-02 18:11:03 -0800

    Add more complication, still doesn't work. Super confused and shooting in the dark.

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,60 @@
 $(document).ready(function() {
+  controller = new Controller;
+});
+
+// Model
+function Die() {
+  this.value = 0;
+
+  this.roll = function(){
+  return Math.floor((Math.random()* 6)+1);
+  };
+}
+
+// View
+function View(dice){  
+  this.displayDie = function(dice) {
+  $('.dice').append('<div class="die">'+ dice.value +'</div>');
+  };
+
+  this.updateDie = function(die) {
+    $(die).text(die.value);
+  }
+}
+
+// Controller
+function Controller(){
+  this.dice = [];
+  this.view = new View;
+  var that = this;
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    new_die = new Die();
+    that.dice.push(new_die);
+    // new_view = new View(new_die);
+    that.view.displayDie(new_die);
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
+    $.each(that.dice, function(index, die){
+      that.view.updateDie(die.roll());
     });
   });
-});
+    // $('.die').each(index, die){
+    //   die.roll
+    // };
+    // new_view = new View(new_die);
+    // new_view.displayDie(new_die);
+  // });
+}
+
+ // $('#roller button.add').on('click', function() {
+ //    console.log("WAT")
+ //    $('.dice').append('<div class="die">0</div>');
+ //  });
+
+ //  $('#roller button.roll').on('click', function() {
+ //    $('.die').each(function(k, die) {
+ //      var value = Math.floor((Math.random()*6)+1);
+ //      $(die).text(value);
+ //    });
+ //  });
